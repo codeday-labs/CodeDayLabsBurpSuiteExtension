92641adee1afeea64e25e200ddbe3f358f62dfe2
Taylor Papke <taylorpapke@gmail.com>
2022-09-07 11:49:07 -0700

    added 2nd dictionary and cleaned up code

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -14,18 +14,31 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
     private final String[] httpName = {"HTTP Response"};
     private final DefaultTableModel apiTableModel = new DefaultTableModel(apiName, 0);
     private final DefaultTableModel httpTableModel = new DefaultTableModel(httpName, 0);
-    private final JTable apiModelJTable = new JTable(apiTableModel);
-    private final JTable httpResponseJTable = new JTable(httpTableModel);
+    private final JTable apiModelJTable = new JTable(apiTableModel)
+    {
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+    };
+    private final JTable httpResponseJTable = new JTable(httpTableModel)
+    {
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+    };
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
+    private JSplitPane splitPaneLeftAndRight;
     private JSplitPane splitPaneLeft;
     private JSplitPane splitPaneRight;
-    private JSplitPane splitPaneLeftContainer;
     private IMessageEditor requestViewer;
     private IMessageEditor responseViewer;
     private IHttpRequestResponse currentlyDisplayedItem;
+    private Integer apiTableSelectedRow = 0;
+    private Integer httpTableSelectedRow = 0;
 
-    private final Dictionary<String, LogEntryDict> dict = new Hashtable<>();
+    private final Dictionary<String, String> dictApi = new Hashtable<>();
+    private final Dictionary<String, LogEntryDict> dictHttp = new Hashtable<>();
 
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
@@ -34,15 +47,18 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                splitPaneLeftContainer = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+                apiModelJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                httpResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+                splitPaneLeft = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
                 splitPaneRight = new JSplitPane(JSplitPane.VERTICAL_SPLIT); //right component
-                splitPaneLeftContainer.setDividerLocation(300);
+                splitPaneLeft.setDividerLocation(300);
 
                 // table of log entries on the left side of the split pane
                 JScrollPane apiLogScrollPane = new JScrollPane(apiModelJTable);
                 JScrollPane httpResponseScrollPane = new JScrollPane(httpResponseJTable);
-                splitPaneLeftContainer.setTopComponent(apiLogScrollPane);
-                splitPaneLeftContainer.setBottomComponent(httpResponseScrollPane);
+                splitPaneLeft.setTopComponent(apiLogScrollPane);
+                splitPaneLeft.setBottomComponent(httpResponseScrollPane);
 
                 // tabs with request/response viewers on the right side of the split pane
                 JTabbedPane rightPaneTabs = new JTabbedPane();
@@ -51,10 +67,12 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
                 rightPaneTabs.addTab("Request", requestViewer.getComponent());
                 rightPaneTabs.addTab("Response", responseViewer.getComponent());
                 splitPaneRight.setRightComponent(rightPaneTabs);
-                splitPaneLeft = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, splitPaneLeftContainer, splitPaneRight);
+
+                //Everything that's displayed in the tab.
+                splitPaneLeftAndRight = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, splitPaneLeft, splitPaneRight);
 
                 // customize our UI components
-                callbacks.customizeUiComponent(splitPaneLeft);
+                callbacks.customizeUiComponent(splitPaneLeftAndRight);
                 callbacks.customizeUiComponent(splitPaneRight);
                 callbacks.customizeUiComponent(apiModelJTable);
                 callbacks.customizeUiComponent(httpResponseJTable);
@@ -76,9 +94,13 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
 
     @Override
     public Component getUiComponent() {
-        return splitPaneLeft;
+        return splitPaneLeftAndRight;
     }
 
+    /*
+        I think what we need to do here is just actually process the message. That is to say parse it out, and call the
+        other method(s) to do their part.
+    */
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
         // only process responses
         if (!messageIsRequest) {
@@ -101,15 +123,14 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
                     *
                     * Right now this is just placeholder info (It's the same data). */
             int row1 = httpResponseJTable.getRowCount();
-            LogEntry le1 = new LogEntry(toolFlag, callbacks.saveBuffersToTempFiles(messageInfo),
-                    helpers.analyzeRequest(messageInfo).getUrl());
             String response = new String(le.requestResponse.getResponse(), StandardCharsets.UTF_8);
             String[] responseTokens = response.split(" ");
             String parsedResponse = responseTokens[0] + " " + responseTokens[1];
             StringBuilder sbHttp = new StringBuilder();
 
             LogEntryDict logDict = new LogEntryDict(parsedResponse, messageInfo);
-            dict.put(parsedRequest, logDict);
+            dictApi.put(request, parsedRequest);
+            dictHttp.put(parsedRequest, logDict);
 
             sbHttp.append(parsedResponse);
             sbHttp.append(" ");
@@ -123,12 +144,29 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
             httpTableModel.fireTableRowsInserted(row1, row1);
 
             // Update the tabs. Need to get the logic for this right. Not currently updating correctly.
-            requestViewer.setMessage(dict.get(parsedRequest).requestResponse.getRequest(), true);
-            //requestViewer.setMessage(le1.requestResponse.getRequest(), true);
-            responseViewer.setMessage(dict.get(parsedRequest).requestResponse.getResponse(), false);
-//            responseViewer.setMessage(le1.requestResponse.getResponse(), false);
-            currentlyDisplayedItem = dict.get(parsedRequest).requestResponse;
-//            currentlyDisplayedItem = le1.requestResponse;
+            requestViewer.setMessage(dictHttp.get(parsedRequest).requestResponse.getRequest(), true);
+            responseViewer.setMessage(dictHttp.get(parsedRequest).requestResponse.getResponse(), false);
+            currentlyDisplayedItem = dictHttp.get(parsedRequest).requestResponse;
+        }
+        setSelectedData(messageInfo);
+    }
+
+    // find out what to display. Called by the processHttpMessage and getValueAt.
+    public void setSelectedData(IHttpRequestResponse messageInfo){
+
+        // Change happened in the top left pane.
+        //Need to figure out the logic on updating tables here based on the dictionaries.
+        if(apiTableSelectedRow != apiModelJTable.getSelectedRow()){
+            apiTableSelectedRow = apiModelJTable.getSelectedRow();
+            httpTableSelectedRow = 0;
+        }
+        // See if anything has changed in bottom left pane.
+        else{
+            /*/Select the data from the top left pane as key, and with the list of values in the bottom
+               select the one that corresponds to the currently selected value (httpTableSelectedRow), and display
+               the information relevant to it.
+            * */
+
         }
     }
 
@@ -151,8 +189,6 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
             public Object getValueAt(int rowIndex, int columnIndex){
                 Object logEntry = apiModelJTable.getValueAt(rowIndex, columnIndex);
                 if (columnIndex == 0) {
-//        String s = new String(logEntry.requestResponse.getRequest(), StandardCharsets.UTF_8);
-//        return s; //callbacks.getToolName(logEntry.tool);
                     return logEntry;
                 } else {
                     return "";
