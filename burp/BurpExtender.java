3028050c8eda6b06380a0d68d0125d3b1750b4ea
Paul T. Maack <paultmaack@gmail.com>
2022-09-11 09:22:19 -0700

    Removed AbstractTableModel extends statement from BurpExtender, integrated maps. Not functional.

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -1,25 +1,56 @@
+// Debug using java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar burpsuite_community.jar
 package burp;
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.Component;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
-public class BurpExtender extends AbstractTableModel implements IBurpExtender, IHttpListener, ITab, IMessageEditorController {
+//public class BurpExtender extends AbstractTableModel implements IBurpExtender, IHttpListener, ITab, IMessageEditorController {
+public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessageEditorController {
+    private IHttpRequestResponse currentlyDisplayedItem;
     private String[] apiName = {"API Path"};
     private String[] httpName = {"HTTP Response"};
     private DefaultTableModel apiTableModel = new DefaultTableModel(apiName, 0);
     private DefaultTableModel httpTableModel = new DefaultTableModel(httpName, 0);
-    private final JTable apiModelJTable = new JTable(apiTableModel)
+
+    private final JTable apiResponseJTable = new JTable(apiTableModel)
     {
         public boolean isCellEditable(int row, int column) {
             return false;
+        }
+        @Override
+        public Object getValueAt(int row, int column) {
+            try {
+                if(apiTableModel.getRowCount() > 0){ setSelectedData();}
+            }
+            catch(Exception e){
+                return super.getValueAt(row, column);
+            }
+            return super.getValueAt(row, column);
         }
     };
     private final JTable httpResponseJTable = new JTable(httpTableModel)
     {
         public boolean isCellEditable(int row, int column) {
             return false;
+        }
+        public Object getValueAt(int row, int column) {
+            try {
+                if(apiTableModel.getRowCount() > 0){ setSelectedData();}
+            }
+            catch(Exception e){
+                return super.getValueAt(row, column);
+            }
+            return super.getValueAt(row, column);
         }
     };
     private IBurpExtenderCallbacks callbacks;
@@ -29,29 +60,43 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
     private JSplitPane splitPaneLeft;
     private IMessageEditor requestViewer;
     private IMessageEditor responseViewer;
-    private IHttpRequestResponse currentlyDisplayedItem;
-    private Integer apiTableSelectedRow = 0;
-    private Integer httpTableSelectedRow = 0;
 
+    private int apiTableSelectedRow = 0;
+    private int httpTableSelectedRow = 0;
+    private StringBuilder sb = new StringBuilder();
 
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+    private HashMap<String, String> apiMap = new HashMap<String, String>();
+    private HashMap<String, List<apiListItem>> httpMap = new HashMap<String, List<apiListItem>>();;
 
+    public void registerExtenderCallbacks(@NotNull IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
         helpers = callbacks.getHelpers();
         callbacks.setExtensionName("sample extension");
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                apiModelJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                httpResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
+                apiResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//                apiModelJTable.setRowSelectionInterval(0,0);
+                apiResponseJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+                    public void valueChanged(ListSelectionEvent event) {
+                        setSelectedData();
+                    }
+                });
                 httpResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                //httpResponseJTable.setRowSelectionInterval(0,0);
+                httpResponseJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+                    public void valueChanged(ListSelectionEvent event) {
+                        setSelectedData();
+                    }
+                });
+                //httpResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
                 splitPaneLeft = new JSplitPane(JSplitPane.VERTICAL_SPLIT); //Left Component
                 splitPaneRight = new JSplitPane(JSplitPane.VERTICAL_SPLIT); //Right component
                 splitPaneLeft.setDividerLocation(300);
 
                 // table of log entries on the left side of the split pane
-                JScrollPane apiLogScrollPane = new JScrollPane(apiModelJTable);
+                JScrollPane apiLogScrollPane = new JScrollPane(apiResponseJTable);
                 JScrollPane httpResponseScrollPane = new JScrollPane(httpResponseJTable);
                 splitPaneLeft.setTopComponent(apiLogScrollPane);
                 splitPaneLeft.setBottomComponent(httpResponseScrollPane);
@@ -70,7 +115,7 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
                 // customize our UI components
                 callbacks.customizeUiComponent(splitPaneLeftAndRight);
                 callbacks.customizeUiComponent(splitPaneRight);
-                callbacks.customizeUiComponent(apiModelJTable);
+                callbacks.customizeUiComponent(apiResponseJTable);
                 callbacks.customizeUiComponent(httpResponseJTable);
                 callbacks.customizeUiComponent(apiLogScrollPane);
                 callbacks.customizeUiComponent(httpResponseScrollPane);
@@ -99,67 +144,89 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
     */
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
         // only process responses
+
         if (!messageIsRequest) {
+            String apiKey = processAPI_Key(messageInfo);
+            String apiValue = processAPI_Value(apiKey, messageInfo);
+            processAPIKeyValuePair(apiKey,apiValue);
+            processHTTPKeyValuePair(apiValue, messageInfo);
 
-            int row = apiModelJTable.getRowCount();
+            int row = apiTableModel.getRowCount();
+            //Maybe don't do this next part. We need to evaluate when we're adding stuff, and if it should be unique.
+            apiTableModel.addRow(new Object[]{apiKey});
+            apiTableModel.fireTableRowsInserted(row, row+1);
 
-            LogEntry le = new LogEntry(toolFlag, callbacks.saveBuffersToTempFiles(messageInfo),
-                    helpers.analyzeRequest(messageInfo).getUrl());
-
-            StringBuilder sb = new StringBuilder();
-
-            sb.append(callbacks.getToolName(le.tool));
-            sb.append(helpers.analyzeRequest(messageInfo).getUrl());
-            sb.append(" ");
-            sb.append(le.url);
-            sb.append(" ");
-            String strEntry = sb.toString();
-            apiTableModel.addRow(new Object[]{strEntry});
-            apiTableModel.fireTableRowsInserted(row, row);
-
-            /*We need to modify the section below here to have logic on selection of the item to display on right
-             * pane so we can pass the call to it from the first log. There's a few ways to go about this.
-             *
-             * Right now this is just placeholder info (It's the same data). */
-            //int row1 = httpResponseJTable.getRowCount();
-            LogEntry le1 = new LogEntry(toolFlag, callbacks.saveBuffersToTempFiles(messageInfo),
-                    helpers.analyzeRequest(messageInfo).getUrl());
-            sb.append(callbacks.getToolName(le1.tool));
-
-                /*
-                Careful. I've got this next line set up to be the same string as above. Once we go to
-                start making this useful we'll need to update that. This whole section should be changed.
-                * */
-
-            httpTableModel.addRow(new Object[]{strEntry});
-            fireTableRowsInserted(row, row);
-
-            /* Update the tabs. This is where we put the info we get from the lookup operation.
-                this can probably go in the same method we have (setSelectedData) to make it happen.
-            */
-            requestViewer.setMessage(le1.requestResponse.getRequest(), true);
-            responseViewer.setMessage(le1.requestResponse.getResponse(), false);
-            currentlyDisplayedItem = le1.requestResponse;
         }
-        setSelectedData(messageInfo);
+        setSelectedData();
     }
 
     // find out what to display. Called by the processHttpMessage and getValueAt.
-    public void setSelectedData(IHttpRequestResponse messageInfo){
-
-        // Change happened in the top left pane.
-        //Need to figure out the logic on updating tables here based on the dictionaries.
-        if(apiTableSelectedRow != apiModelJTable.getSelectedRow()){
-            apiTableSelectedRow = apiModelJTable.getSelectedRow();
+    public void setSelectedData(){
+        boolean hasChanged = false;
+        if(apiTableSelectedRow != apiResponseJTable.getSelectedRow() && apiResponseJTable.getSelectedRow() > 0) {
+            hasChanged = true;
+            apiTableSelectedRow = apiResponseJTable.getSelectedRow();
             httpTableSelectedRow = 0;
         }
-        // See if anything has changed in bottom left pane.
+        if(!hasChanged && httpTableSelectedRow > 0 ){
+            httpTableSelectedRow = httpResponseJTable.getSelectedRow();
+        }
         else{
-            /*/Select the data from the top left pane as key, and with the list of values in the bottom
-               select the one that corresponds to the currently selected value (httpTableSelectedRow), and display
-               the information relevant to it.
-            * */
+            httpTableSelectedRow = 0; // Default. When we don't know what else is going on.
+        }
+        String key = apiTableModel.getValueAt(apiTableSelectedRow,0).toString();
+        List<apiListItem> Value = httpMap.get(apiMap.get(key));
+        httpTableModel.setRowCount(0);
+        for (apiListItem apli : Value) {
+            httpTableModel.addRow(new Object[]{apli.key});
+        }
+        httpTableModel.fireTableRowsInserted(0, httpTableModel.getRowCount());
+        setSelectedHTTP(httpTableSelectedRow);
+    }
 
+    public void setSelectedHTTP(int row){
+        //httpResponseJTable.setRowSelectionInterval(row,row);
+        setTabs(httpTableModel.getValueAt(row,0).toString());
+    }
+    public void setTabs(String key){
+        requestViewer.setMessage(httpMap.get(key).get(httpTableSelectedRow).messageInfo.getRequest(), true);
+        responseViewer.setMessage(httpMap.get(key).get(httpTableSelectedRow).messageInfo.getResponse(), false);
+    }
+    public String processAPI_Key(@NotNull IHttpRequestResponse messageInfo){
+        sb.setLength(0);
+        String request = new String(messageInfo.getRequest(), StandardCharsets.UTF_8);
+        String[] requestTokens = request.split(" ");
+        sb.append(requestTokens[0]);
+        sb.append(" ");
+        sb.append(requestTokens[1]);
+        String apiKey = sb.toString();
+        sb.setLength(0);
+        return apiKey;
+    }
+    public String processAPI_Value(String key, @NotNull IHttpRequestResponse messageInfo){
+        String response = new String(messageInfo.getResponse(), StandardCharsets.UTF_8);
+        String[] responseTokens = response.split(" ");
+        sb.append(key);
+        sb.append(" ");
+        sb.append(responseTokens[1]);
+        String apiValue = sb.toString();
+        sb.setLength(0);
+        return apiValue;
+    }
+    public void processAPIKeyValuePair(String key, String value){
+        if(!apiMap.containsKey(key)){
+            apiMap.put(key,value);
+        }
+    }
+    public void processHTTPKeyValuePair(String key, IHttpRequestResponse messageInfo){
+    apiListItem temp = new apiListItem(key, messageInfo);
+        if(!httpMap.containsKey(key)){
+            List<apiListItem> ali = new ArrayList<>();
+            ali.add(temp);
+            httpMap.put(key, ali);
+        }
+        else{
+            httpMap.get(key).add(temp);
         }
     }
 
@@ -177,34 +244,9 @@ public class BurpExtender extends AbstractTableModel implements IBurpExtender, I
     public IHttpService getHttpService() {
         return currentlyDisplayedItem.getHttpService();
     }
-
-
-    /* We need to look at overriding this so we can call the custom method we use for processing messages. Maybe.
-     */
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        Object logEntry = apiModelJTable.getValueAt(rowIndex, columnIndex);
-        if (columnIndex == 0) {
-//        String s = new String(logEntry.requestResponse.getRequest(), StandardCharsets.UTF_8);
-//        return s; //callbacks.getToolName(logEntry.tool);
-            return logEntry;
-        } else {
-            return "";
-        }
+    private record LogEntry(int tool, IHttpRequestResponse requestResponse, URL url) {
     }
 
-    private record LogEntry(int tool, IHttpRequestResponsePersisted requestResponse, URL url) {
+    private record apiListItem(String key, IHttpRequestResponse messageInfo){
     }
-
-    // Don't do anything with these. They're artifacts from a necessary (currently) thing we can't get rid of.
-    @Override
-    public int getRowCount() {
-        return 0;
-    }
-
-    @Override
-    public int getColumnCount() {
-        return 0;
-    }
-    // End artifacts. We may end up using those, but I have no purpose for them at the moment.
 }
