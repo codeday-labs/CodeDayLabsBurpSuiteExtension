bd74e29e051aba838ce12eb0deaadd637df509a7
Paul T. Maack <paultmaack@gmail.com>
2022-09-12 13:40:20 -0700

    Got the logic right. Fully functional, only minor bug.

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -1,55 +1,43 @@
 // Debug using java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar burpsuite_community.jar
 package burp;
 import org.jetbrains.annotations.NotNull;
-
 import java.awt.Component;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import javax.swing.*;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
-//public class BurpExtender extends AbstractTableModel implements IBurpExtender, IHttpListener, ITab, IMessageEditorController {
 public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessageEditorController {
+// Start Variables
     private IHttpRequestResponse currentlyDisplayedItem;
     private String[] apiName = {"API Path"};
     private String[] httpName = {"HTTP Response"};
     private DefaultTableModel apiTableModel = new DefaultTableModel(apiName, 0);
     private DefaultTableModel httpTableModel = new DefaultTableModel(httpName, 0);
-
-    private final JTable apiResponseJTable = new JTable(apiTableModel)
-    {
+    private final JTable apiResponseJTable = new JTable(apiTableModel) {
         public boolean isCellEditable(int row, int column) {
             return false;
         }
         @Override
         public Object getValueAt(int row, int column) {
-            try {
-                if(apiTableModel.getRowCount() > 0){ setSelectedData();}
-            }
-            catch(Exception e){
-                return super.getValueAt(row, column);
-            }
             return super.getValueAt(row, column);
         }
     };
-    private final JTable httpResponseJTable = new JTable(httpTableModel)
-    {
+    private final JTable httpResponseJTable = new JTable(httpTableModel) {
         public boolean isCellEditable(int row, int column) {
             return false;
         }
         public Object getValueAt(int row, int column) {
-            try {
-                if(apiTableModel.getRowCount() > 0){ setSelectedData();}
-            }
-            catch(Exception e){
-                return super.getValueAt(row, column);
-            }
             return super.getValueAt(row, column);
         }
     };
@@ -60,14 +48,22 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessag
     private JSplitPane splitPaneLeft;
     private IMessageEditor requestViewer;
     private IMessageEditor responseViewer;
-
     private int apiTableSelectedRow = 0;
     private int httpTableSelectedRow = 0;
     private StringBuilder sb = new StringBuilder();
+    private HashMap<String, List<apiListItem>> dictionaryMapThing = new HashMap<String, List<apiListItem>>();
+// End Variables
 
-    private HashMap<String, String> apiMap = new HashMap<String, String>();
-    private HashMap<String, List<apiListItem>> httpMap = new HashMap<String, List<apiListItem>>();;
+// Start Record
+    /*
+     Used as members of a list, where key is the line displayed on the HTTP response table, and messageInfo
+     Is the stuff that's in the tabs when that is selected.
+     */
+    private record apiListItem(String key, IHttpRequestResponse messageInfo) {
+    }
+// End Records
 
+//Start No Touchy
     public void registerExtenderCallbacks(@NotNull IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
         helpers = callbacks.getHelpers();
@@ -75,22 +71,22 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessag
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-
                 apiResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-//                apiModelJTable.setRowSelectionInterval(0,0);
-                apiResponseJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+                apiResponseJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
                     public void valueChanged(ListSelectionEvent event) {
                         setSelectedData();
                     }
                 });
+                apiResponseJTable.setRowSelectionAllowed(true);
+                apiResponseJTable.setColumnSelectionAllowed(false);
                 httpResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-                //httpResponseJTable.setRowSelectionInterval(0,0);
-                httpResponseJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+                httpResponseJTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
                     public void valueChanged(ListSelectionEvent event) {
                         setSelectedData();
                     }
                 });
-                //httpResponseJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                httpResponseJTable.setRowSelectionAllowed(true);
+                httpResponseJTable.setColumnSelectionAllowed(false);
                 splitPaneLeft = new JSplitPane(JSplitPane.VERTICAL_SPLIT); //Left Component
                 splitPaneRight = new JSplitPane(JSplitPane.VERTICAL_SPLIT); //Right component
                 splitPaneLeft.setDividerLocation(300);
@@ -127,72 +123,52 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessag
             }
         });
     }
+//End No Touchy
 
-    @Override
-    public String getTabCaption() {
-        return "DTP Updated";
-    } //Dipendra, Taylor & Paul. ;)
 
-    @Override
-    public Component getUiComponent() {
-        return splitPaneLeftAndRight;
-    }
-
+// Process everything.
     /*
         I think what we need to do here is just actually process the message. That is to say parse it out, and call the
         other method(s) to do their part.
     */
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
-        // only process responses
-
         if (!messageIsRequest) {
+            // get the key and value
             String apiKey = processAPI_Key(messageInfo);
             String apiValue = processAPI_Value(apiKey, messageInfo);
-            processAPIKeyValuePair(apiKey,apiValue);
-            processHTTPKeyValuePair(apiValue, messageInfo);
-
-            int row = apiTableModel.getRowCount();
-            //Maybe don't do this next part. We need to evaluate when we're adding stuff, and if it should be unique.
-            apiTableModel.addRow(new Object[]{apiKey});
-            apiTableModel.fireTableRowsInserted(row, row+1);
-
+            processDataSources(apiKey, apiValue, messageInfo);
+            if(dictionaryMapThing.keySet().size() > 0){
+                setSelectedData();
+            }
         }
+    }
+
+    public void processDataSources(String key, String value, IHttpRequestResponse messageInfo) {
+        apiListItem temp = new apiListItem(value, messageInfo);
+        // If it's not in the dictionary, add it to the dictionary and the API list.
+        if (!dictionaryMapThing.containsKey(key)) {
+            int size = apiTableModel.getRowCount();
+            List<apiListItem> ali = new ArrayList<>();
+            ali.add(temp);
+            dictionaryMapThing.put(key, ali);
+            apiTableModel.addRow(new Object[]{key});
+            apiTableModel.fireTableRowsInserted(size, apiTableModel.getRowCount());
+        }
+        else { //Already in the dictionary.
+        // If the current message key is the same as the selected key, we need to also add the value to the http list...
+            dictionaryMapThing.get(key).add(temp);//
+        }
+        apiResponseJTable.setRowSelectionInterval(apiTableSelectedRow,apiTableSelectedRow);
+        setHTTPTable();
         setSelectedData();
     }
 
-    // find out what to display. Called by the processHttpMessage and getValueAt.
-    public void setSelectedData(){
-        boolean hasChanged = false;
-        if(apiTableSelectedRow != apiResponseJTable.getSelectedRow() && apiResponseJTable.getSelectedRow() > 0) {
-            hasChanged = true;
-            apiTableSelectedRow = apiResponseJTable.getSelectedRow();
-            httpTableSelectedRow = 0;
-        }
-        if(!hasChanged && httpTableSelectedRow > 0 ){
-            httpTableSelectedRow = httpResponseJTable.getSelectedRow();
-        }
-        else{
-            httpTableSelectedRow = 0; // Default. When we don't know what else is going on.
-        }
-        String key = apiTableModel.getValueAt(apiTableSelectedRow,0).toString();
-        List<apiListItem> Value = httpMap.get(apiMap.get(key));
-        httpTableModel.setRowCount(0);
-        for (apiListItem apli : Value) {
-            httpTableModel.addRow(new Object[]{apli.key});
-        }
-        httpTableModel.fireTableRowsInserted(0, httpTableModel.getRowCount());
-        setSelectedHTTP(httpTableSelectedRow);
-    }
 
-    public void setSelectedHTTP(int row){
-        //httpResponseJTable.setRowSelectionInterval(row,row);
-        setTabs(httpTableModel.getValueAt(row,0).toString());
-    }
-    public void setTabs(String key){
-        requestViewer.setMessage(httpMap.get(key).get(httpTableSelectedRow).messageInfo.getRequest(), true);
-        responseViewer.setMessage(httpMap.get(key).get(httpTableSelectedRow).messageInfo.getResponse(), false);
-    }
-    public String processAPI_Key(@NotNull IHttpRequestResponse messageInfo){
+// End process everything.
+
+// Process API (Top left table) stuff.
+
+    public String processAPI_Key(@NotNull IHttpRequestResponse messageInfo) {
         sb.setLength(0);
         String request = new String(messageInfo.getRequest(), StandardCharsets.UTF_8);
         String[] requestTokens = request.split(" ");
@@ -203,7 +179,8 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessag
         sb.setLength(0);
         return apiKey;
     }
-    public String processAPI_Value(String key, @NotNull IHttpRequestResponse messageInfo){
+
+    public String processAPI_Value(String key, @NotNull IHttpRequestResponse messageInfo) {
         String response = new String(messageInfo.getResponse(), StandardCharsets.UTF_8);
         String[] responseTokens = response.split(" ");
         sb.append(key);
@@ -213,23 +190,72 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessag
         sb.setLength(0);
         return apiValue;
     }
-    public void processAPIKeyValuePair(String key, String value){
-        if(!apiMap.containsKey(key)){
-            apiMap.put(key,value);
+// End Process API Stuff.
+
+// Process HTTP (Bottom Left Table) Stuff.
+
+    private  void setHTTPTable(){
+        int index = 0;
+        //API Selection Hasn't Changed.
+        if(httpTableModel.getRowCount() > 0){
+            index = httpTableModel.getRowCount();
+        }
+        //only add in new elements.
+        for (int i = index; i < dictionaryMapThing.get(apiResponseJTable.getValueAt(apiTableSelectedRow,0).toString()).size() ; i++) {
+            String value = dictionaryMapThing.get(apiResponseJTable.getValueAt(apiTableSelectedRow,0).toString()).get(i).key;
+            httpTableModel.addRow(new Object[]{value});
+            httpTableModel.fireTableRowsInserted(index, httpTableModel.getRowCount());
         }
     }
-    public void processHTTPKeyValuePair(String key, IHttpRequestResponse messageInfo){
-    apiListItem temp = new apiListItem(key, messageInfo);
-        if(!httpMap.containsKey(key)){
-            List<apiListItem> ali = new ArrayList<>();
-            ali.add(temp);
-            httpMap.put(key, ali);
-        }
-        else{
-            httpMap.get(key).add(temp);
-        }
+// End Process HTTP Stuff.
+
+// Process Tabs (The component on the right, with the full message info)
+
+    public void setTabs() {
+        requestViewer.setMessage(dictionaryMapThing.get(apiTableModel.getValueAt(apiTableSelectedRow,0)).get(httpTableSelectedRow).messageInfo.getRequest(), true);
+        responseViewer.setMessage(dictionaryMapThing.get(apiTableModel.getValueAt(apiTableSelectedRow,0)).get(httpTableSelectedRow).messageInfo.getResponse(), false);
     }
 
+// End Process Tabs
+    //Do GUI Update Stuff.
+    // find out what to display. Called by the processHttpMessage and getValueAt.
+public void setSelectedData() {
+// If we don't have data, we can't do anything.
+    if(dictionaryMapThing.keySet().size() > 0){
+        boolean hasChanged = false;
+        //API section
+        //Selected and "last selected" not the same.
+        if(apiTableSelectedRow != apiResponseJTable.getSelectedRow()){
+            hasChanged = true;
+            //First entry will be -1
+            if(apiResponseJTable.getSelectedRow() >= 0){
+                apiTableSelectedRow = apiResponseJTable.getSelectedRow();
+            }
+            apiResponseJTable.setRowSelectionInterval(apiTableSelectedRow,apiTableSelectedRow);
+        }
+        //HTTP Section
+        //API Table changed
+        if(hasChanged){ // Always set the HTTP table to zero, because it's a new table...
+            httpTableSelectedRow = 0;
+            httpTableModel.setRowCount(0);
+            setHTTPTable();
+            httpResponseJTable.setRowSelectionInterval(httpTableSelectedRow,httpTableSelectedRow);
+        }
+        // API Table didn't change.
+        else{
+            //Change in the HTTP table index
+            //First entry will be -1
+            if(httpResponseJTable.getSelectedRow() >= 0){
+                httpTableSelectedRow = httpResponseJTable.getSelectedRow();
+            }
+        }
+        setHTTPTable();
+        setTabs();
+    }
+}
+// End GUI Stuff.
+
+    // Functions for interpreting data that's sent from the Burp software.
     @Override
     public byte[] getRequest() {
         return currentlyDisplayedItem.getRequest();
@@ -244,9 +270,18 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IMessag
     public IHttpService getHttpService() {
         return currentlyDisplayedItem.getHttpService();
     }
-    private record LogEntry(int tool, IHttpRequestResponse requestResponse, URL url) {
-    }
+// End Functions for interpreting data that's sent from the Burp software.
 
-    private record apiListItem(String key, IHttpRequestResponse messageInfo){
+    // Functions for displaying the tab on Burp.
+    // The tab's name.
+    @Override
+    public String getTabCaption() {
+        return "DTP Updated";
+    } //Dipendra, Taylor & Paul. ;)
+
+    // The GUI component (everything that's displayed)
+    @Override
+    public Component getUiComponent() {
+        return splitPaneLeftAndRight;
     }
 }
