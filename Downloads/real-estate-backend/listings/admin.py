f9ab0add30aed5cfbf8190a8f906c0d9de3685db
taylorpapke <94101953+taylorpapke@users.noreply.github.com>
2025-06-17 20:20:29 -0700

    Delete Downloads/real-estate-backend directory

--- Downloads/real-estate-backend/listings/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import SupabaseListing
-
-@admin.register(SupabaseListing)
-class SupabaseListingAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in SupabaseListing._meta.fields]
